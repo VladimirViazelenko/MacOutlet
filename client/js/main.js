8019c6b2099ffa92525dbadf909bf29da5967113
Volodymyr Viazelenko <vviazelenko@gmail.com>
2022-12-04 20:20:38 +0200

    corrected links

--- client/js/main.js
+++ client/js/main.js
@@ -2,7 +2,7 @@ window.history.pushState("object or string", "MacOutlet - Main", "/user/main");
 let productService = [];
 (async () => {
     const authToken = await cookieStore.get('token'); 
-    fetch('https://macoutletshop.herokuapp.com/user/products', {
+    fetch('http://localhost:3000/user/products', {
     method: 'GET', 
     headers: {
         "x-access-token": authToken.value,
